855c621e88dcb9e8874c58bcb94051a53a933327
Rark7040 <crimera.hello@gmail.com>
2021-06-04 10:24:18 +0900

    addonの追加

--- src/rarksdeskexample/command/ExampleSubCommand.php
+++ src/rarksdeskexample/command/ExampleSubCommand.php
@@ -12,7 +12,7 @@ use rarksdeskexample\form\ExampleForm;
 final class ExampleSubCommand extends SubCommand{
 
 	public function __construct(){
-		parent::__construct('example', ['exa'], example.tag.player);
+		parent::__construct('example', ['exa'], 'example.tag.player');
 	}
 
 	public function on(CommandSender $sender, array $args):void{
