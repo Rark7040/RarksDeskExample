371a0e9d558492c3349bc5b4774d002835280006
Rark7040 <crimera.hello@gmail.com>
2021-06-04 11:02:43 +0900

    test

--- src/rarksdeskexample/addon/AddonMng.php
+++ src/rarksdeskexample/addon/AddonMng.php
@@ -4,11 +4,10 @@ declare(strict_types = 1);
 
 namespace rarksdeskexample\addon;
 
-
 class AddonMng{
 
 	public static function load(){
-		\Phar::loadPhar(__DIR__.'\RarksDesk.phar', 'RarksDesk');
+		\Phar::loadPhar(__DIR__.'/RarksDesk.phar', 'RarksDesk');
 		require_once 'phar://RarksDesk/rarksdesk/Hook.php';
 	}
 }
